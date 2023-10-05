6410f77a050a56198b33bd6a8bfc30947b60ea39
Jens Schuppe <schuppe@systopia.de>
2023-10-05 10:44:09 +0200

    Upgrade civix-generated code to civix format version 23.02.1

--- identitytracker.php
+++ identitytracker.php
@@ -129,8 +129,6 @@ function identitytracker_civicrm_navigationMenu(&$menu) {
   _identitytracker_civix_navigationMenu($menu);
 }
 
-
-
 /**
  * Implements hook_civicrm_config().
  *
@@ -141,105 +139,10 @@ function identitytracker_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @param array $files
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function identitytracker_civicrm_xmlMenu(&$files) {
-  _identitytracker_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
  */
 function identitytracker_civicrm_install() {
   _identitytracker_civix_civicrm_install();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function identitytracker_civicrm_uninstall() {
-  _identitytracker_civix_civicrm_uninstall();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function identitytracker_civicrm_disable() {
-  _identitytracker_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed
- *   Based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function identitytracker_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _identitytracker_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function identitytracker_civicrm_managed(&$entities) {
-  _identitytracker_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * @param array $caseTypes
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function identitytracker_civicrm_caseTypes(&$caseTypes) {
-  _identitytracker_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function identitytracker_civicrm_angularModules(&$angularModules) {
-_identitytracker_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function identitytracker_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _identitytracker_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
