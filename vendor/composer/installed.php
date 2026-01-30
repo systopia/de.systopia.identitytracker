47c3fb32a8b98e6a1abe4f6d47057436a89c1734
Jens Schuppe <schuppe@systopia.de>
2026-01-30 12:26:14 +0100

    Set version to 1.5.0-beta1

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'systopia/de.systopia.identitytracker',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '264cb4b7fdaea3fbba8f046905c9d5789873bb44',
+        'type' => 'civicrm-ext',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'systopia/de.systopia.identitytracker' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '264cb4b7fdaea3fbba8f046905c9d5789873bb44',
+            'type' => 'civicrm-ext',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
