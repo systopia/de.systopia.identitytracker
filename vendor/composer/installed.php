f8278e7c0adc19ebb8fb17986b1298732299b43e
Sven Nowotnik <nowotnik@systopia.de>
2026-06-22 09:09:14 +0200

    Set version to 1.6.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,27 @@
+<?php return array(
+    'root' => array(
+        'name' => 'systopia/de.systopia.identitytracker',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '87d86a0679e2766b3659a8ca0ca4b770e2078a5c',
+        'type' => 'civicrm-ext',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(
+            0 => '1.6.x-dev',
+        ),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'systopia/de.systopia.identitytracker' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '87d86a0679e2766b3659a8ca0ca4b770e2078a5c',
+            'type' => 'civicrm-ext',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(
+                0 => '1.6.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
